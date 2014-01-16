3c052b1a413ad956805fed593cdfb70f4e4b3e83
abhijeet <abhijeet.t91@gmail.com>
2014-01-15 19:42:08 -0500

    added new data

--- calibration/getData.m
+++ calibration/getData.m
@@ -1,7 +1,7 @@
-function readings = getData(rob,p)
-% return 100 readings collected from pixel p
-readings = zeros(length(p),100);
-for i = 1:100;
+function readings = getData(rob,num_readings,p)
+% return num_readings collected from pixel p
+readings = zeros(length(p),num_readings);
+for i = 1:num_readings;
     readings(:,i) = rob.laser.data.ranges(p);
     pause(0.3);
 end
