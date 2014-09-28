f23427c62e47297b3ea67fd6f9f22cbcc92e8849
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2014-09-28 13:21:05 -0400

    Working on registration covariance.

--- code/exp8/getRegCov.m
+++ code/exp8/getRegCov.m
@@ -1,16 +1,20 @@
 clearAll
 load reg_cov_map
-load processed_data_sep27
+load('processed_data_sep27','poses')
 load reg_cov_perturbations
 
 numScans = 50;
 nPoses = size(poses,2);
 
-%%
+%% Compute covariance
 load reg_cov_real_scans
-i = 1;
+S = cell(1,nPoses);
 fprintf('Stats for real...\n');
 t1 = tic();
-wgl = getWiggliness(scans{i},poses(:,i),map,perturbations);
-S = getPoseCovariance([wgl.pEst]);
+for i = 1
+    wgl = getWiggliness(scans{i},poses(:,i),map,perturbations);
+    S{i} = getPoseCovariance([wgl.pEst]);
+end
 fprintf('Computation took %.2fs.\n',toc(t1));
+
+
