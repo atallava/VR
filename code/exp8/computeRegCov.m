2fd1e91cde0323489955c7144d07ad1eabf58efe
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2014-09-27 23:14:59 -0400

    Noise data for different models.

--- code/exp8/computeRegCov.m
+++ code/exp8/computeRegCov.m
@@ -11,16 +11,18 @@ function [bias,S] = computeRegCov(poses,scans,map)
 % S     - Covariance of residuals.
 
 localizer = lineMapLocalizer(map.objects);
-refiner = laserPoseRefiner(struct('localizer',localizer,'laser',robotModel.laser,'skip',5,'numIterations',300));
+refiner = laserPoseRefiner(struct('localizer',localizer,'laser',robotModel.laser,'skip',5,'numIterations',500));
 
 count = 1;
 nPoses = size(poses,2);
 for i = 1:nPoses
     nScans = size(scans{i},1);
+    poseRef = poses(:,i);
+    [~,poseRef] = refiner.refine(scans{i}(1,:),poseRef);
     for j = 1:nScans
-        [~,pEst] = refiner.refine(scans{i}(j,:),poses(:,i));
-        residuals(:,count) = poses(:,i)-pEst;
-        residuals(3,count) = thDiff(pEst(3),poses(3,i));
+        [~,pEst] = refiner.refine(scans{i}(j,:),poseRef);
+        residuals(:,count) = poseRef-pEst;
+        residuals(3,count) = thDiff(pEst(3),poseRef(3));
         count = count+1;
     end
 end
--- code/exp8/computeSimRegCov.m
+++ code/exp8/computeSimRegCov.m
@@ -1,6 +1,6 @@
 clearAll
 load reg_cov_map
-load reg_cov_robot_poses
+load('processed_data_sep27','poses')
 
 numScans = 50;
 nPoses = size(poses,2);
@@ -12,7 +12,7 @@ fprintf('Stats for sim...\n');
 for i = 1:nPoses
     scans{i} = generateScansAtState(rsim,poses(:,i),map,numScans);
 end
-[bias,S] = computeRegCov(poses(:,1),scans,map);
+[bias,S] = computeRegCov(poses,scans,map);
 save('sim_reg_cov','bias','S');
 fprintf('Computation took %.2fs...\n',toc(t1));
 
@@ -24,6 +24,6 @@ fprintf('Stats for baseline...\n');
 for i = 1:nPoses
     scans{i} = generateScansAtState(rsim,poses(:,i),map,numScans);
 end
-[bias,S] = computeRegCov(poses(:,1),scans,map);
+[bias,S] = computeRegCov(poses,scans,map);
 save('baseline_reg_cov','bias','S');
 fprintf('Computation took %.2fs...\n',toc(t1));
