65636fdffbc63edcd3bc0cb3e8696585d4634780
abhijeet <abhijeet.t91@gmail.com>
2014-09-29 04:15:59 -0400

    Updated exp8 mats with B100 data.

--- code/exp8/generateSampleTrajectories.m
+++ code/exp8/generateSampleTrajectories.m
@@ -1,22 +1,21 @@
-load nsh3_corridor
+load b100_padded_corridor
 load motion_filter_object
 
 localizer = lineMapLocalizer(map.objects);
 sampleTrajectories = struct('poseArray',{},'tArray',{});
 count = 1;
 distThresh = 0.01^2;
-while count <= 50
+while count <= 40
     [poseArray,tArray] = mfl.sampleTrajectory();
-    
-    d = localizer.closestSquaredDistanceToLines(poseArray);
-    % Throw away trajectory if it is too close to corridor walls.
-    if min(d) <= distThresh
-        continue;
-    else
-        sampleTrajectories(count).poseArray = poseArray;
-        sampleTrajectories(count).tArray = tArray;
-        count = count+1;
-    end
+	
+	%     d = localizer.closestSquaredDistanceToLines(poseArray);
+	%     % Throw away trajectory if it is too close to corridor walls.
+	%     if min(d) <= distThresh
+	%         continue;
+	%     end
+	sampleTrajectories(count).poseArray = poseArray;
+	sampleTrajectories(count).tArray = tArray;
+	count = count+1;
 end
 
 
--- code/exp8/generateSimScansAtSamples.m
+++ code/exp8/generateSimScansAtSamples.m
@@ -11,7 +11,8 @@ for i = 1:numTrajectories
     [scanPoseArray,tArray] = scanPosesFromSampleTrajectory(sampleTrajectories(i).poseArray,sampleTrajectories(i).tArray);
     scanArray = cell(1,size(scanPoseArray,2));
     for j = 1:size(scanPoseArray,2)
-        scanArray{j} = rsim.simulate(scanPoseArray(:,j));
+		laserPose = robotModel.laser.refPoseToLaserPose(scanPoseArray(:,j));
+        scanArray{j} = rsim.simulate(laserPose);
     end
     sensorData(end+1).scanArray = scanArray;
     sensorData(end).tArray = tArray;
@@ -28,7 +29,8 @@ for i = numTrajectories+1:2*numTrajectories
     [scanPoseArray,tArray] = scanPosesFromSampleTrajectory(sampleTrajectories(i).poseArray,sampleTrajectories(i).tArray);
     scanArray = cell(1,size(scanPoseArray,2));
     for j = 1:size(scanPoseArray,2)
-        scanArray{j} = rsim.simulate(scanPoseArray(:,j));
+		laserPose = robotModel.laser.refPoseToLaserPose(scanPoseArray(:,j));
+        scanArray{j} = rsim.simulate(laserPose);
     end
     sensorData(end+1).scanArray = scanArray;
     sensorData(end).tArray = tArray;
