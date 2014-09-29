d1909515466a1540c4a1e82c615a0d7f1d943dca
abhijeet <abhijeet.t91@gmail.com>
2014-09-29 03:15:14 -0400

    Updated motion noise with new data.

--- code/exp8/motionCov.m
+++ code/exp8/motionCov.m
@@ -1,10 +1,10 @@
-load linear_motion_data
+load angular_motion_data
 nPoses = size(poseStart,2);
 [encResidual,commandResidual] = deal(zeros(size(poseStart)));
 for i = 1:nPoses
     posePredicted = poseStart(:,i);
-    posePredicted(1) = posePredicted(1)+0.2;
-    %posePredicted(3) = posePredicted(3)+pi/2;
+    %posePredicted(1) = posePredicted(1)+0.2;
+    posePredicted(3) = posePredicted(3)+pi/2;
     commandResidual(:,i) = poseFinal(:,i)-posePredicted;
     commandResidual(3,i) = thDiff(posePredicted(3),poseFinal(3,i));
     encResidual(:,i) = poseFinal(:,i)-poseEncFinal(:,i);
