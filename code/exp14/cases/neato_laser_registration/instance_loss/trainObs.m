310b167892632cca2de843f0bf45d1d2a9983c8a
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2016-01-07 18:02:17 -0500

    exp14. Added neato_laser_registration.

---
+++ code/exp14/cases/neato_laser_registration/instance_loss/trainObs.m
@@ -0,0 +1,12 @@
+% train model on observation risk
+
+%% setup variables
+% specify dataset
+% specify loss
+
+%% setup model
+% big question
+
+%% optimize
+% some risk function
+fun = @(x) modelObjAlgodev(x,laserModel,algosVars);
